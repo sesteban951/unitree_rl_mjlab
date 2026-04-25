e930add4281c680c922828c4da5292dc3617c2c1
sesteban951 <sesteban@caltech.edu>
2026-04-24 18:00:40 -0700

    added 23dof mimic custom env with no state estimation and pelvis anchor

--- src/tasks/tracking/config/g1_23dof/__init__.py
+++ src/tasks/tracking/config/g1_23dof/__init__.py
@@ -1,7 +1,10 @@
 from mjlab.tasks.registry import register_mjlab_task
 from src.tasks.tracking.rl import MotionTrackingOnPolicyRunner
 
-from .env_cfgs import unitree_g1_23dof_flat_tracking_env_cfg
+from .env_cfgs import (
+  unitree_g1_23dof_flat_tracking_env_cfg,
+  unitree_g1_23dof_pelvis_tracking_env_cfg,
+)
 from .rl_cfg import unitree_g1_23dof_tracking_ppo_runner_cfg
 
 register_mjlab_task(
@@ -16,6 +19,18 @@ register_mjlab_task(
   task_id="Unitree-G1-23Dof-Tracking-No-State-Estimation",
   env_cfg=unitree_g1_23dof_flat_tracking_env_cfg(has_state_estimation=False),
   play_env_cfg=unitree_g1_23dof_flat_tracking_env_cfg(has_state_estimation=False, play=True),
+  rl_cfg=unitree_g1_23dof_tracking_ppo_runner_cfg(),
+  runner_cls=MotionTrackingOnPolicyRunner,
+)
+
+##################################################################
+# CUSTOM W/ PELVIS & NO STATE ESTIMATION
+##################################################################
+
+register_mjlab_task(
+  task_id="Unitree-G1-23Dof-Tracking-Custom",
+  env_cfg=unitree_g1_23dof_pelvis_tracking_env_cfg(),
+  play_env_cfg=unitree_g1_23dof_pelvis_tracking_env_cfg(play=True),
   rl_cfg=unitree_g1_23dof_tracking_ppo_runner_cfg(),
   runner_cls=MotionTrackingOnPolicyRunner,
 )
